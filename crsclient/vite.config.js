e8285191a34edfcb0c92e8aa0da78c94b3a50b82
Ricardo Rodriguez <peyotex@gmail.com>
2022-02-14 00:06:27 -0600

    Small ui

--- crsclient/vite.config.js
+++ crsclient/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// https://github.com/xiaoluoboding/sql-repl/blob/main/client/package.json
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
